ef9432cb348529fd53778ad27e4a99c34c8b61d0
wp <fromwp2005@163.com>
2022-02-14 15:23:43 +0800

    could capture all videos in directory with specified intervals of frame and output to target directory

---
+++ test.py
@@ -0,0 +1,12 @@
+a = 'abc' + '.jpg'
+print(a)
+
+c = 'abc'
+if c == 'abc':
+    print('c is abc')
+    quit()
+
+
+b = 'frame' + '{:d}'.format(20)+'.jpg'
+print(b)
+
--- video_cap_frame.py
+++ video_cap_frame.py
@@ -1,5 +1,7 @@
 import argparse
+from operator import truediv
 import os
+import cv2
 
 IMAGE_EXT = [".jpg", ".jpeg", ".bmp", ".png"]
 VIDEO_EXT = [".mp4", ".avi"]
@@ -48,8 +50,25 @@ def make_parser():
 
     return parser
 
+def args_check(args):
+    if os.path.isdir(args.output[0]) is False:
+        try:
+            os.makedirs(args.output[0], exist_ok = True)
+            print("Directory '%s' created successfully" % args.output[0])
+        except OSError as error:
+            print("Directory '%s' can not be created" % args.output[0])
+            
+            return 'invalid'
+
+    return 'valid'
+
+
+
+
 if __name__ == "__main__":
     args = make_parser().parse_args()
+    if args_check(args) == 'invalid':
+        quit()
 
     for x in args.input:
         if os.path.isdir(x):
@@ -57,4 +76,17 @@ if __name__ == "__main__":
             for ext in VIDEO_EXT:
                 file = file + [f for f in os.listdir(x) if f.lower().endswith(ext)]
             print(file)
+            for f in file:
+                cap = cv2.VideoCapture(os.path.join(x, f))
+                count = 0
             
+                while cap.isOpened():
+                    ret, frame = cap.read()
+
+                    if ret:
+                        cv2.imwrite(os.path.splitext(os.path.join(args.output[0],f))[0]+'_{:d}'.format(count)+'.jpg', frame)
+                        count += args.period[0] # i.e. at 30 fps, this advances one second
+                        cap.set(cv2.CAP_PROP_POS_FRAMES, count)
+                    else:
+                        cap.release()
+                        break
