97bbc7e06b332d18b79238ce537fc7a8be9dda07
unknown <fromwp2005@163.com>
2022-03-12 13:31:19 +0800

    可得到速度，时间，经纬度，日期等信息。

--- esp32_NMEA_speed/esp32_NMEA_speed.ino
+++ esp32_NMEA_speed/esp32_NMEA_speed.ino
@@ -5,6 +5,8 @@
 #define GPSSerial Serial1
 // Connect to the GPS on the hardware port
 Adafruit_GPS GPS(&GPSSerial);
+#define GPSECHO false
+uint32_t timer = millis();
 
 void setup()
 {
@@ -55,53 +57,81 @@ void setup()
 
 void loop() // run over and over
 {
-  if (GPSSerial.available())
-    Serial.write(GPSSerial.read());
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c)
+      Serial.print(c);
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived())
+  {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    //    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return;                       // we can fail to parse a sentence in which case we should just wait for another
+  }
 
-  // // Now we will start our GPS module, parse (break into parts) the Last NMEA sentence
-  // GPS.parse(GPS.lastNMEA()); // This is going to parse the last NMEA sentence the Arduino has received, breaking it down into its constituent parts.
-  // GPS.newNMEAreceived();     // This will return a boolean TRUE/FALSE depending on the case.
-  //                            // Print the current date/time/etc
-  // Serial.print("\nTime: ");
-  // Serial.print(GPS.hour, DEC);
-  // Serial.print(':');
-  // Serial.print(GPS.minute, DEC);
-  // Serial.print(':');
-  // Serial.print(GPS.seconds, DEC);
-  // Serial.print('.');
-  // Serial.println(GPS.milliseconds);
-  // Serial.print("Date: ");
-  // Serial.print(GPS.day, DEC);
-  // Serial.print('/');
-  // Serial.print(GPS.month, DEC);
-  // Serial.print("/20");
-  // Serial.println(GPS.year, DEC);
-  // Serial.print("Fix: ");
-  // Serial.print((int)GPS.fix);
-  // Serial.print(" quality: ");
-  // Serial.println((int)GPS.fixquality);
-  // if (mySerial.available())
-  //   Serial.write(mySerial.read());
-  // // If GPS module has a fix, line by line prints the GPS information
-  // if (GPS.fix)
-  // {
-  //   Serial.print("Location: ");
-  //   Serial.print(GPS.latitude, 4);
-  //   Serial.print(GPS.lat);
-  //   Serial.print(", ");
-  //   Serial.print(GPS.longitude, 4);
-  //   Serial.println(GPS.lon);
-  //   Serial.print("Location (in degrees, works with Google Maps): ");
-  //   Serial.print(GPS.latitudeDegrees, 4);
-  //   Serial.print(", ");
-  //   Serial.println(GPS.longitudeDegrees, 4);
-  //   Serial.print("Speed (knots): ");
-  //   Serial.println(GPS.speed);
-  //   Serial.print("Angle: ");
-  //   Serial.println(GPS.angle);
-  //   Serial.print("Altitude: ");
-  //   Serial.println(GPS.altitude);
-  //   Serial.print("Satellites: ");
-  //   Serial.println((int)GPS.satellites);
-  // }
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000)
+  {
+    timer = millis(); // reset the timer
+    Serial.print("\nTime: ");
+    if (GPS.hour < 10)
+    {
+      Serial.print('0');
+    }
+    Serial.print(GPS.hour, DEC);
+    Serial.print(':');
+    if (GPS.minute < 10)
+    {
+      Serial.print('0');
+    }
+    Serial.print(GPS.minute, DEC);
+    Serial.print(':');
+    if (GPS.seconds < 10)
+    {
+      Serial.print('0');
+    }
+    Serial.print(GPS.seconds, DEC);
+    Serial.print('.');
+    if (GPS.milliseconds < 10)
+    {
+      Serial.print("00");
+    }
+    else if (GPS.milliseconds > 9 && GPS.milliseconds < 100)
+    {
+      Serial.print("0");
+    }
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC);
+    Serial.print('/');
+    Serial.print(GPS.month, DEC);
+    Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: ");
+    Serial.print((int)GPS.fix);
+    Serial.print(" quality: ");
+    Serial.println((int)GPS.fixquality);
+    if (GPS.fix)
+    {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4);
+      Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4);
+      Serial.println(GPS.lon);
+      Serial.print("Speed (knots): ");
+      Serial.println(GPS.speed);
+      Serial.print("Angle: ");
+      Serial.println(GPS.angle);
+      Serial.print("Altitude: ");
+      Serial.println(GPS.altitude);
+      Serial.print("Satellites: ");
+      Serial.println((int)GPS.satellites);
+    }
+  }
 }
