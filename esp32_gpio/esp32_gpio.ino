2408cdb5869e238aa7c889d88401d496fca11ba5
unknown <fromwp2005@163.com>
2022-03-18 10:41:28 +0800

    buzzer需要继续研究

--- esp32_gpio/esp32_gpio.ino
+++ esp32_gpio/esp32_gpio.ino
@@ -1,12 +1,53 @@
-#define LED    2
+#define BUZZER 10
 
-void setup() {
-  pinMode(LED, OUTPUT);
+const int PWM_Ch = 0;
+const int PWM_Res = 8;
+const int PWM_Freq = 1000;
+const int DutyCycle = 50;
+
+void setup()
+{
+  ledcAttachPin(BUZZER, PWM_Ch);
 }
 
-void loop() {
-  digitalWrite(LED, 0);
+void loop()
+{
+  ledcAttachPin(BUZZER, PWM_Ch);
+
+  ledcSetup(PWM_Ch, PWM_Freq, PWM_Res);
+  ledcWrite(PWM_Ch, 10);
+  delay(300);
+  ledcWrite(PWM_Ch, 50);
+  delay(300);
+  ledcWrite(PWM_Ch, 90);
+  delay(300);
+
+  ledcWrite(PWM_Ch, 0);
+  delay(3000);
+
+  ledcSetup(PWM_Ch, 5000, PWM_Res);
+  ledcWrite(PWM_Ch, 10);
+  delay(300);
+  ledcWrite(PWM_Ch, 50);
+  delay(300);
+  ledcWrite(PWM_Ch, 90);
+  delay(300);
+
+  ledcWrite(PWM_Ch, 0);
+  delay(3000);
+
+  ledcSetup(PWM_Ch, PWM_Freq, PWM_Res);
+  ledcWrite(PWM_Ch, 50);
+  delay(300);
+  ledcSetup(PWM_Ch, 2000, PWM_Res);
+  ledcWrite(PWM_Ch, 50);
+  delay(300);
+
+  ledcWriteTone(PWM_Ch, 1000);
   delay(1000);
-  digitalWrite(LED, 1);
-  delay(2000);
+  ledcWriteTone(PWM_Ch, 2000);
+  delay(1000);
+
+  ledcDetachPin(BUZZER);
+  delay(6000);
 }
