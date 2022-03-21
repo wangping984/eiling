edf6eed1c91df08784eaf65c50bb5a6adab2d814
unknown <fromwp2005@163.com>
2022-03-21 08:14:30 +0800

    找到easybuzzer库声音难听的原因了。update（）中，每次循环，只要还在响声，就重新设置ledcAttachPin();和ledcWriteTone();导致声音质量极差。

--- esp32_gpio/esp32_gpio.ino
+++ esp32_gpio/esp32_gpio.ino
@@ -8,46 +8,47 @@ const int DutyCycle = 50;
 void setup()
 {
   ledcAttachPin(BUZZER, PWM_Ch);
+  ledcSetup(PWM_Ch, PWM_Freq, PWM_Res);
 }
 
 void loop()
 {
   ledcAttachPin(BUZZER, PWM_Ch);
-
-  ledcSetup(PWM_Ch, PWM_Freq, PWM_Res);
-  ledcWrite(PWM_Ch, 10);
-  delay(300);
-  ledcWrite(PWM_Ch, 50);
-  delay(300);
-  ledcWrite(PWM_Ch, 90);
-  delay(300);
-
-  ledcWrite(PWM_Ch, 0);
-  delay(3000);
-
-  ledcSetup(PWM_Ch, 5000, PWM_Res);
-  ledcWrite(PWM_Ch, 10);
-  delay(300);
-  ledcWrite(PWM_Ch, 50);
-  delay(300);
-  ledcWrite(PWM_Ch, 90);
-  delay(300);
-
-  ledcWrite(PWM_Ch, 0);
-  delay(3000);
-
-  ledcSetup(PWM_Ch, PWM_Freq, PWM_Res);
-  ledcWrite(PWM_Ch, 50);
-  delay(300);
-  ledcSetup(PWM_Ch, 2000, PWM_Res);
-  ledcWrite(PWM_Ch, 50);
-  delay(300);
+//
+//  
+//  ledcWrite(PWM_Ch, 10);
+//  delay(300);
+//  ledcWrite(PWM_Ch, 50);
+//  delay(300);
+//  ledcWrite(PWM_Ch, 90);
+//  delay(300);
+//
+//  ledcWrite(PWM_Ch, 0);
+//  delay(3000);
+//
+//  ledcSetup(PWM_Ch, 5000, PWM_Res);
+//  ledcWrite(PWM_Ch, 10);
+//  delay(300);
+//  ledcWrite(PWM_Ch, 50);
+//  delay(300);
+//  ledcWrite(PWM_Ch, 90);
+//  delay(300);
+//
+//  ledcWrite(PWM_Ch, 0);
+//  delay(3000);
+//
+//  ledcSetup(PWM_Ch, PWM_Freq, PWM_Res);
+//  ledcWrite(PWM_Ch, 50);
+//  delay(300);
+//  ledcSetup(PWM_Ch, 2000, PWM_Res);
+//  ledcWrite(PWM_Ch, 50);
+//  delay(300);
 
   ledcWriteTone(PWM_Ch, 1000);
-  delay(1000);
-  ledcWriteTone(PWM_Ch, 2000);
-  delay(1000);
-
-  ledcDetachPin(BUZZER);
-  delay(6000);
+//  delay(1000);
+//  ledcWriteTone(PWM_Ch, 2000);
+//  delay(1000);
+//
+//  ledcDetachPin(BUZZER);
+//  delay(6000);
 }
